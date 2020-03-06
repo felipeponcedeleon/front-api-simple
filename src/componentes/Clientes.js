609bd20f50e9401f42796dfaa806d9eadc08ac65
felipeponcedeleonespinosa <59258794+felipeponcedeleonespinosa@users.noreply.github.com>
2020-03-06 01:22:41 -0400

    Vista Clientes - Listar clientes de la db

--- src/componentes/Clientes.js
+++ src/componentes/Clientes.js
@@ -1,8 +1,49 @@
-import React from 'react';
+//HOOKS useEffect ahora se utiliza en vez de
+//componentDidMount() y componentWillMount()
+import React, { useEffect, useState, Fragment } from 'react';
+
+//import cliente axios
+import clienteAxios from '../config/axios';
+
+//Componentes
+import Cliente from './Cliente';
 
 function Clientes() {
+
+    //State
+    //clientes = state
+    //guardarClientes = setState
+    const [clientes, guardarClientes] = useState([]);
+
+    //Llamando a la API
+    //Todos los clientes
+    const consultarAPI = async() => {
+        const clientesConsulta = await clienteAxios.get('/clientes');
+        //console.log(clientesConsulta.data);
+
+        //Colocar resutado en el state
+        guardarClientes(clientesConsulta.data);
+    }
+
+    //equivalente a componentDidMount() y componentWillMount()
+    useEffect(() => {
+        consultarAPI();
+    }, []);
+    //arreglo vacío para ejecutarse solo una vez
+
     return(
-        <h2>Clientes</h2>
+        <Fragment>
+            <h2>Clientes</h2>
+
+            <ul className="listado-clientes">
+                {clientes.map(cliente => (
+                    <Cliente
+                        key={cliente._id}
+                        cliente={cliente}
+                    />
+                ))}
+            </ul>
+        </Fragment>
     );
 }
 
